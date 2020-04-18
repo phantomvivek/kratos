2460f331dbe70a3910308ce4c6a8612e92047847
Vivek Shah <vivek.shah@uber.com>
2020-04-18 21:42:48 +0530

    Added config example folder

---
+++ example_test/sample_ws_server/sample-ws.js
@@ -0,0 +1,32 @@
+let WebSocketServer = require('uws').Server;
+let express = require('express')
+let http = require('http')
+let connectCount = 0;
+let totalConnects = 1;
+
+let app = express();
+let server = http.createServer(app);
+let wss = new WebSocketServer({ server: server, clientTracking: false });
+
+server.listen("8080", function ()
+{
+    console.log("Server started")
+});
+wss.on('connection', Connect)
+
+function Connect(ws) {
+    let socketId = totalConnects;
+    connectCount++;
+    totalConnects++;
+    console.log(`\nConnection opened: ${socketId}`)
+    ws.on('message', (msg) => {
+        console.log(`Message on socket ${socketId}: ${msg}`)
+    });
+    ws.on('close', () => {
+        connectCount--;
+    });
+    ws.on('error', (err) => {
+        console.log("Error", err)
+        connectCount--
+    });
+}
